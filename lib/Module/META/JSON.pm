9501c0f49d255003c4b02da82cce98444e961fa3
jcap <git@jcap.me>
2009-05-11 17:12:33 -0400

    initial import

---
+++ lib/Module/META/JSON.pm
@@ -0,0 +1,38 @@
+package Module::META::JSON;
+use strict;
+use vars qw[$VERSION];
+$VERSION = '0.01';
+
+use JSON;
+use IO::All;
+use base qw[Module::META];
+
+sub read {
+    my ($class, $file) = @_;
+    my $load = jsonToObj(io($file)->all);
+    return $class->new(%{$load});
+}
+
+__END__
+
+=head1 NAME
+
+Module::META - Generic Representation of META.yml 1.0 Spec
+
+=head1 SYNOPSIS
+
+  use Module::META;
+
+  my $meta = Module::META->new(
+      name           => 'Module-Name',
+      build_requires => {
+          'Test::More' => '0.46',
+      },
+  );
+  $meta->abstract("Module with a Name");
+
+=head1 SEE ALSO
+
+Reference document, L<http://module-build.sourceforge.net/META-spec-new.html>.
+
+=cut
